a50d27b1b22132c0e9e167c3dd210d621a4786d2
KMMX05 <alejandromercado@Mac-mini-de-KMMX05.local>
2018-03-03 14:10:15 -0600

    clase python 03/03/01

---
+++ diccionario.py
@@ -0,0 +1,4 @@
+diccionario = {'jugadores': "oribe", 'DT': "piojo", 'estadio':"azteca", 'televisora':"televisa", 'patrocinadores':"coca" }
+print diccionario['jugadores']
+print diccionario['DT']
+print diccionario['estadio']
