0bc0b5ceefadd644fc99499304dd9a6fbfbf761c
KMMX05 <alejandromercado@Mac-mini-de-KMMX05.local>
2018-03-31 12:58:47 -0600

    RFC

--- RFC.py
+++ RFC.py
@@ -1,50 +1,128 @@
 #/usr/bin/python 
 #-*-encoding: utf-8 -*-
 
-Fname =raw_input("primer nombre: " )
-Sname =raw_input("segundo nombre: ")
-apellido=raw_input("apellido paterno: ")
-apellidom=raw_input("apellido materno: ")
-dia=raw_input("dia de nacimiento: ")
-mes=raw_input("mes de nacimiento: "	)
-year = raw_input("year: ")
-lista=['puto','culo', 'pipi','cola','pedo', 'mogJ']
+while True:
+	Fname =raw_input("primer nombre: " )
+	if len(Fname) > 0 and Fname.isalpha() :
+		break 
+	else:
+		print ("\033[32merror datos inv\033[m")
+
+
+while True:
+	Sname =raw_input("segundo nombre: " )
+	if len(Sname) > 0 and Sname.isalpha() :
+		break 
+	else:
+		print ("\033[32merror datos inv\033[m")
+
+
+
+while True:
+	apellido=raw_input("apellido paterno: " )
+	if len(apellido) > 0 and apellido.isalpha() :
+		break 
+	else:
+		print ("\033[32merror datos inv\033[m")
+
+
+
+while True:
+	apellidom=raw_input("apellido materno: " )
+	if len(apellidom) > 0 and apellidom.isalpha() :
+		break 
+	else:
+		print ("\033[32merror datos inv\033[m")
+
+
+while True:
+	dia=raw_input("dia de nacimiento: ")
+	if len(dia) > 0 and dia.isdigit() :
+		break 
+	else:
+		print ("\033[32merror datos inv\033[m")
+
+
+
+while True:
+	mes=raw_input("mes de nacimiento: ")
+	if len(mes) > 0 and mes.isdigit() :
+		break 
+	else:
+		print ("\033[32merror datos inv\033[m")
+
+
+
+while True:
+	year=raw_input("anhio de nacimiento: ")
+	if len(year) > 0 and year.isdigit() :
+		break 
+	else:
+		print ("\033[32merror datos inv\033[m")
+
+
 
 MFname= Fname.upper()
 
+#SI ES JOSE, TOMA EL SEGUNDO NOMBRE, SI NO HAY SEGUNDO, TOMA EL PRIMERO.
 if len(Sname)==0:
 	name2= MFname[:1]
-elif MFname=="JOSE":
+elif MFname=="JOSE" or "MARIA":
 	name2=Sname[:1] 
 else: 
 	name2= Sname[:1]
 
-if len(dia)==1:
+#AGREGA UN DIGITO AL DIA, SI LO NESECITA y 
+if  len(dia)==1:
 	dia2= "0" + str(dia)
 else: 
-	dia2=dia
+	dia2= "0" + str(dia)
 
-if len(mes)==1:
-	mes2= "0" + str(mes)
-else: 
+#AGREGA UN DIGITO AL MES, SI LO NESECITA.
+if len(mes)==1 :
+		mes2= "0" + str(mes)
+else:
 	mes2=mes
+
+#SOLO USA LOS DOS ULTIMOS DIGITOS DEL ANHIO
 
 if len(year)==4:
 	year2= year[2:]
 else: 
 	year2=year
 
-x=apellido[:2]
-y=apellidom[:1]
-z=name2
+x=apellido[:2].upper()
+y=apellidom[:1].upper()
+z=name2.upper()
 anio=year2
 RFCx= x + y + z
-#point= lista.index(RFCx)
-print RFCx in lista 
+
+#REVISA EL RFC, QUE NO TENGO PALABRAS PROHIBIDAS.
+
+lista=['PUTO','CULO', 'PIPI','COLA','pedo', 'MOGY']
+
 check= RFCx in lista
 
-#if RFCx
-#RFC= RFCx + dia2 + mes2 + anio + "xxx"
+if check == True:
+	x1=x[:1] + "X"
+else:
+	x1=x
 
+RFCx1= x1 + y + z
+
+RFC= RFCx1 + dia2 + mes2 + anio + "xxx"
+
+
+print ("\033[32m" + RFC + "\033[0m")
  
-#print RFC.upper()
+
+
+
+
+
+
+
+
+
+
+
